c779cd56b524ee71c47cf03f084a2fad579e4679
Lin <13871876+tianlinxu312@users.noreply.github.com>
2020-06-04 06:42:36 +0100

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -34,7 +34,7 @@ class Logger(object):
                 s = BytesIO()
             Image.fromarray(img).save(s, format="png")
             
-            img_sum = tf.summary.image(encoded_image_string=s.getvalue())
+            img_sum = s.getvalue())
             
             img_summaries.append(img_sum)
 
